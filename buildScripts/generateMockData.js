33d91c96b7742de0cb0b0177ac920f410d632ed5
idkjs <aarmand.inbox@gmail.com>
2017-04-19 17:02:46 +0200

    fixed syntax errors in schema, changed json-faker version number to 4.01, fixed babel in wstorm

--- buildScripts/generateMockData.js
+++ buildScripts/generateMockData.js
@@ -7,13 +7,13 @@
 /* eslint-disable no-console */
 
 import jsf from 'json-schema-faker';
-import {schema} from './mockDataSchema';
+import { schema } from './mockDataSchema';
 import fs from 'fs';
 import chalk from 'chalk';
 
 /* Call JSON.stringify on results of jsf (json schema faker) */
 const json = JSON.stringify(jsf(schema));
-console.log(json);
+// console.log(json);
 fs.writeFile("./src/api/db.json", json, function (err) {
   if (err) {
     return console.log(chalk.red(err));
@@ -21,3 +21,17 @@ fs.writeFile("./src/api/db.json", json, function (err) {
     console.log(chalk.green("Mock data generated."));
   }
 });
+//
+// const jsf = require('json-schema-faker');
+// const mockDataSchema = require('./mockDataSchema');
+// const fs = require('fs');
+//
+// const json = JSON.stringify(jsf(mockDataSchema));
+//
+// fs.writeFile("./src/api/db.json", json, function (err) {
+//   if (err) {
+//     return console.log(err);
+//   } else {
+//     console.log("Mock data generated.");
+//   }
+// });
