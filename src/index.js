bae79afd7f2df34835387063dda9fe3ebe4421ec
idkjs <aarmand.inbox@gmail.com>
2017-04-20 10:15:48 +0200

    end of Module 11. Delete works.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 /**
  * Created by devworx on 4/17/17.
  */
-import {getUsers} from './api/userApi'
+import {getUsers, deleteUser} from './api/userApi'
 
 // Populate table of users via API call.
 getUsers().then(result => {
-  let usersBody = ""
-
+  let usersBody = "";
   result.forEach(user => {
     usersBody+= `<tr>
       <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
@@ -17,5 +16,22 @@ getUsers().then(result => {
       </tr>`
   })
   global.document.getElementById('users').innerHTML = usersBody
+
+  // create var to hold all delete links selected off the DOM by getElementsByClassName
+  const deleteLinks = global.document.getElementsByClassName('deleteUser')
+
+  /* Must use array.from to create a real array from a DOM collection b/c getElementByClassName
+  * only returns an "array like" object
+  * */
+
+  Array.from(deleteLinks, link => {
+    link.onclick =  (event) => {
+      const element = event.target
+      event.preventDefault()
+      deleteUser(element.attributes["data-id"].value)
+      const row = element.parentNode.parentNode
+      row.parentNode.removeChild(row)
+    }
+  })
 })
 
