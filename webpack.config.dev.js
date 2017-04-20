07c0c08d02ae9ca7b1ddc49cce672a985ec8babd
idkjs <aarmand.inbox@gmail.com>
2017-04-20 15:08:22 +0200

    updated remaining webpack configs to wp2. working. Module 13.3

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,9 +1,12 @@
-/**
- * Created by devworx on 4/17/17.
- */
+
+import webpack from 'webpack';
 import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.json']
+  },
   devtool: 'inline-source-map',
   entry: [
     path.resolve(__dirname, 'src/index')
@@ -14,7 +17,18 @@ export default {
     publicPath: '/',
     filename: 'bundle.js'
   },
-  plugins: [],
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: false,
+      debug: true,
+      noInfo: true // set to false to see a list of every file being bundled.
+    }),
+    // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    })
+  ],
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,30 +1,103 @@
-/**
- * Created by devworx on 4/17/17.
- */
+// /**
+//  * Created by devworx on 4/17/17.
+//  */
+import webpack from 'webpack';
 import path from 'path';
-import * as webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
-  devtool: 'inline-source-map',
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.json']
+  },
+  devtool: 'source-map',
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
   target: 'web',
   output: {
-    path: path.resolve(__dirname, 'src'),
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
-    // Eliminate duplicate packages when generating bundle
-    new webpack.optimize.DedupePlugin(),
+    // Global loader configuration
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+      noInfo: true // set to false to see a list of every file being bundled.
+    }),
+
+    // Generate an external css file with a hash in the filename
+    new ExtractTextPlugin('[name].[contenthash].css'),
+
+    // Hash the files using MD5 so that their names change when the content changes.
+    new WebpackMd5Hash(),
+
+    // Use CommonsChunkPlugin to create a separate bundle
+    // of vendor libraries so that they're cached separately.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+
+    // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true,
+      // Properties you define here are available in index.html
+      // using htmlWebpackPlugin.options.varName
+      trackJSToken: 'INSERT YOUR TOKEN HERE'
+    }),
+
     // Minify JS
     new webpack.optimize.UglifyJsPlugin()
   ],
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
-      {test: /\.css$/, loaders: ['style-loader','css-loader']}
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css-loader?sourceMap')}
     ]
   }
-}
+};
+// import path from 'path';
+// import webpack from 'webpack'
+//
+// export default {
+//   devtool: 'source-map',
+//   context: path.resolve(__dirname, './src'),
+//   entry: {
+//     index: './index.js'
+//   },
+//   target: 'web',
+//   output: {
+//     path: path.resolve(__dirname, './dist'),
+//     // publicPath: '/',
+//     filename: 'bundle.js'
+//   },
+//   plugins: [
+//     // Eliminate duplicate packages when generating bundle. Removed from Webpack 2
+//     // new webpack.optimize.DedupePlugin(),
+//     // Minify JS
+//     new webpack.optimize.UglifyJsPlugin()
+//   ],
+//   module: {
+//     loaders: [
+//       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
+//       {test: /\.css$/, loaders: ['style-loader','css-loader']}
+//     ]
+//   }
+// }
