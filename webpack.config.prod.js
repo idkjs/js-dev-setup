69ebef5b83e5950e39b189dcbf84d778fee40d99
idkjs <aarmand.inbox@gmail.com>
2017-04-20 15:50:45 +0200

    webpack bundle splitting working. Module 13.7

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -73,31 +73,4 @@ export default {
     ]
   }
 };
-// import path from 'path';
-// import webpack from 'webpack'
-//
-// export default {
-//   devtool: 'source-map',
-//   context: path.resolve(__dirname, './src'),
-//   entry: {
-//     index: './index.js'
-//   },
-//   target: 'web',
-//   output: {
-//     path: path.resolve(__dirname, './dist'),
-//     // publicPath: '/',
-//     filename: 'bundle.js'
-//   },
-//   plugins: [
-//     // Eliminate duplicate packages when generating bundle. Removed from Webpack 2
-//     // new webpack.optimize.DedupePlugin(),
-//     // Minify JS
-//     new webpack.optimize.UglifyJsPlugin()
-//   ],
-//   module: {
-//     loaders: [
-//       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
-//       {test: /\.css$/, loaders: ['style-loader','css-loader']}
-//     ]
-//   }
-// }
+
